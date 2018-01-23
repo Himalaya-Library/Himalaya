5c100971248855e62352e0f8a90ff74b4219e350
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-01-23 14:40:28 +0100

    compare EFT calculation with H3m at 1-loop level

--- test/test_lambda_normalization.cpp
+++ test/test_lambda_normalization.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include "HierarchyCalculator.hpp"
+#include "Mh2EFTCalculator.hpp"
 
 namespace {
 
@@ -15,15 +16,17 @@ himalaya::Parameters make_point()
    const double beta = std::atan(tb);
    const double v = 245.;
 
+   const double eps = 0.001;
+
    himalaya::Parameters pars;
    pars.scale = MS;
    pars.mu    = MS;
    pars.g3    = 1.05733;
    pars.vu    = v*std::sin(beta);
    pars.vd    = v*std::cos(beta);
-   pars.mq2   << pow2(MS), 0, 0, 0, pow2(MS), 0, 0, 0, pow2(MS);
+   pars.mq2   << pow2(MS), 0, 0, 0, pow2(MS), 0, 0, 0, pow2(MS)*(1 + eps);
    pars.md2   << pow2(MS), 0, 0, 0, pow2(MS), 0, 0, 0, pow2(MS);
-   pars.mu2   << pow2(MS), 0, 0, 0, pow2(MS), 0, 0, 0, pow2(MS);
+   pars.mu2   << pow2(MS), 0, 0, 0, pow2(MS), 0, 0, 0, pow2(MS)*(1 - eps);
    pars.At    = xt*MS + pars.mu/tb;
    pars.Ab    = 0;
    pars.MG    = MS;
@@ -36,13 +39,15 @@ himalaya::Parameters make_point()
    return pars;
 }
 
-/// calculates Mh^2 in the decoupling limit
-double calc_Mh2_tree_dec(const himalaya::Parameters& pars)
+// calculates a_t
+double calc_at(const himalaya::Parameters& pars)
 {
-   const double beta = std::atan(pars.vu/pars.vd);
-   const double cos_2beta = std::cos(2*beta);
+   const double Pi = 3.141592653589793;
+   const double yt = std::sqrt(2.)*pars.Mt/pars.vu;
+   const double tb = pars.vu/pars.vd;
+   const double beta = std::atan(tb);
 
-   return pow2(pars.MZ * cos_2beta);
+   return pow2(yt*std::sin(beta))/(4*Pi);
 }
 
 /// calculates lightest mass eigenvalue of given mass matrix
@@ -50,6 +55,31 @@ double calc_Mh2(const Eigen::Matrix2d& Mh_mat)
 {
    const Eigen::SelfAdjointEigenSolver<Eigen::Matrix2d> es(Mh_mat);
    return es.eigenvalues()(0);
+}
+
+/// calculates Mh^2 in the EFT at tree level
+double calc_Mh2_EFT_0L(const himalaya::Parameters& pars)
+{
+   const double beta = std::atan(pars.vu/pars.vd);
+   const double cos_2beta = std::cos(2*beta);
+
+   return pow2(pars.MZ * cos_2beta);
+}
+
+/// calculates Mh^2 in the EFT at 1-loop level
+double calc_Mh2_EFT_1L(const himalaya::Parameters& pars)
+{
+   const double at = calc_at(pars);
+   const double tb = pars.vu/pars.vd;
+   const double mt = pars.Mt;
+   const double mQ32 = pars.mq2(2,2);
+   const double mU32 = pars.mu2(2,2);
+   const double Xt = pars.At - pars.mu/tb;
+   const double MR2 = pow2(pars.scale);
+
+   himalaya::mh2_eft::Mh2EFTCalculator mhc;
+
+   return mhc.Mh2_EFT_1loop(at, mt, mQ32, mU32, Xt, MR2);
 }
 
 } // anonymous namespace
@@ -77,5 +107,8 @@ BOOST_AUTO_TEST_CASE(test_lambda_normalization)
    BOOST_TEST_MESSAGE("Mh(2L) = " << std::sqrt(Mh2_2L));
    BOOST_TEST_MESSAGE("Mh(3L) = " << std::sqrt(Mh2_3L));
 
-   BOOST_CHECK_CLOSE_FRACTION(Mh2_0L, calc_Mh2_tree_dec(pars), 1e-5);
+   BOOST_CHECK_CLOSE_FRACTION(Mh2_0L, calc_Mh2_EFT_0L(pars), 1e-5);
+   BOOST_CHECK_CLOSE_FRACTION(Mh2_1L, calc_Mh2_EFT_0L(pars)
+                                    + calc_Mh2_EFT_1L(pars), 1e-6);
+
 }
