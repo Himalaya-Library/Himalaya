338bbf773093a0ec4ee078faece75b00590bf6f5
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-01-23 15:24:09 +0100

    compare EFT calculation with H3m at 3-loop level

--- test/test_lambda_normalization.cpp
+++ test/test_lambda_normalization.cpp
@@ -7,6 +7,8 @@
 namespace {
 
 double pow2(double x) { return x*x; }
+double pow4(double x) { return x*x*x*x; }
+double pow6(double x) { return x*x*x*x*x*x; }
 
 himalaya::Parameters make_point()
 {
@@ -16,7 +18,7 @@ himalaya::Parameters make_point()
    const double beta = std::atan(tb);
    const double v = 245.;
 
-   const double eps = 0.001;
+   const double eps = 0.01;
 
    himalaya::Parameters pars;
    pars.scale = MS;
@@ -29,7 +31,7 @@ himalaya::Parameters make_point()
    pars.mu2   << pow2(MS), 0, 0, 0, pow2(MS), 0, 0, 0, pow2(MS)*(1 - eps);
    pars.At    = xt*MS + pars.mu/tb;
    pars.Ab    = 0;
-   pars.MG    = MS;
+   pars.MG    = MS*(1 - eps);
    pars.MW    = 74.597;
    pars.MZ    = 85.7704;
    pars.Mt    = 144.337;
@@ -39,11 +41,24 @@ himalaya::Parameters make_point()
    return pars;
 }
 
+// calculates y_t in the MSSM
+double calc_yt(const himalaya::Parameters& pars)
+{
+   return std::sqrt(2.)*pars.Mt/pars.vu;
+}
+
+// calculates y_t in the SM
+double calc_gt(const himalaya::Parameters& pars)
+{
+   const double v = std::sqrt(pow2(pars.vu) + pow2(pars.vd));
+   return std::sqrt(2.)*pars.Mt/v;
+}
+
 // calculates a_t
 double calc_at(const himalaya::Parameters& pars)
 {
    const double Pi = 3.141592653589793;
-   const double yt = std::sqrt(2.)*pars.Mt/pars.vu;
+   const double yt = calc_yt(pars);
    const double tb = pars.vu/pars.vd;
    const double beta = std::atan(tb);
 
@@ -100,6 +115,47 @@ double calc_Mh2_EFT_2L(const himalaya::Parameters& pars)
    return mhc.Mh2_EFT_2loop(at, mt, mQ32, mU32, Xt, MR2, g3, m3);
 }
 
+/// calculates Mh^2 in the EFT at 3-loop level
+double calc_Mh2_EFT_3L(const himalaya::Parameters& pars,
+                       const himalaya::HierarchyObject& ho)
+{
+   const double at = calc_at(pars);
+   const double tb = pars.vu/pars.vd;
+   const double mt = pars.Mt;
+   const double mQ32 = pars.mq2(2,2);
+   const double mU32 = pars.mu2(2,2);
+   const double Xt = pars.At - pars.mu/tb;
+   const double MR2 = pow2(pars.scale);
+   const double g3 = pars.g3;
+   const double m3 = pars.MG;
+   const double msq2 = std::pow(
+      pars.mq2(0,0) * pars.mq2(1,1) *
+      pars.mu2(0,0) * pars.mu2(1,1) *
+      pars.md2(0,0) * pars.md2(1,1), 1./6.);
+   const double gt  = calc_gt(pars);
+   const double v2  = pow2(pars.vu) + pow2(pars.vd);
+   const double v   = std::sqrt(v2);   // ~ 245 GeV
+   const double vDO = v/std::sqrt(2.); // ~ 173 GeV
+
+   himalaya::mh2_eft::Mh2EFTCalculator mhc;
+
+   const double DMh2_EFT_3L_logs =
+      mhc.Mh2_EFT_3loop(at, mt, mQ32, mU32, Xt, MR2, g3, m3, msq2);
+
+   const double zeta_lambda_3L = ho.getZetaDegenerated();
+   const double Pi = 3.141592653589793;
+   const double as = pow2(g3)/(4*pow2(Pi));
+   // prefactor from paper
+   const double pref = pow4(mt)/pow2(4*Pi*vDO)*pow2(as);
+   // prefactor from HSSUSY
+   const double pref2 = 8*pow4(gt*g3)/pow6(4*Pi) * v2;
+   const double DMh2_EFT_3L_const = pref * zeta_lambda_3L;
+
+   BOOST_CHECK_CLOSE_FRACTION(pref, pref2, 1e-10);
+
+   return DMh2_EFT_3L_logs + DMh2_EFT_3L_const;
+}
+
 } // anonymous namespace
 
 BOOST_AUTO_TEST_CASE(test_lambda_normalization)
@@ -126,10 +182,16 @@ BOOST_AUTO_TEST_CASE(test_lambda_normalization)
    BOOST_TEST_MESSAGE("Mh(3L) = " << std::sqrt(Mh2_3L));
 
    BOOST_CHECK_CLOSE_FRACTION(Mh2_0L, calc_Mh2_EFT_0L(pars), 1e-5);
+
    BOOST_CHECK_CLOSE_FRACTION(Mh2_1L, calc_Mh2_EFT_0L(pars)
                                     + calc_Mh2_EFT_1L(pars), 1e-6);
+
    BOOST_CHECK_CLOSE_FRACTION(Mh2_2L, calc_Mh2_EFT_0L(pars)
                                     + calc_Mh2_EFT_1L(pars)
                                     + calc_Mh2_EFT_2L(pars), 1e-6);
 
+   BOOST_CHECK_CLOSE_FRACTION(Mh2_3L, calc_Mh2_EFT_0L(pars)
+                                    + calc_Mh2_EFT_1L(pars)
+                                    + calc_Mh2_EFT_2L(pars)
+                                    + calc_Mh2_EFT_3L(pars,ho), 0.005);
 }
