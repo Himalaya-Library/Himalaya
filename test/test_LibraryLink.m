34807ab647410edbf995123ffd9b97ffb26e5da9
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-05-25 11:07:46 +0200

    generalize test script

--- test/test_LibraryLink.m
+++ test/test_LibraryLink.m
@@ -25,66 +25,99 @@ TestClose[a_, b_, eps_:10^-15] :=
        failedTests++
       ];
 
+TestPoint[point_, name_] :=
+    Module[{output, eps},
+           output = HimalayaCalculateDMh3L[Sequence @@ (input /. point)];
+           eps = precision /. point;
+
+           Print[];
+           Print["==== Testing Point ", name, " ================================"];
+           Print[];
+
+           TestEqual[renormalizationScheme                          /. output, renormalizationScheme                          /. (expectedOutput /. point)];
+           TestEqual[hierarchyID                                    /. output, hierarchyID                                    /. (expectedOutput /. point)];
+           TestEqual[hierarchyName                                  /. output, hierarchyName                                  /. (expectedOutput /. point)];
+           TestClose[Mstop                                          /. output, Mstop                                          /. (expectedOutput /. point), eps];
+           TestClose[Mh2Tree                                        /. output, Mh2Tree                                        /. (expectedOutput /. point), eps];
+           TestClose[Mh21Loop                                       /. output, Mh21Loop                                       /. (expectedOutput /. point), eps];
+           TestClose[Mh22Loop                                       /. output, Mh22Loop                                       /. (expectedOutput /. point), eps];
+           TestClose[Mh23Loop                                       /. output, Mh23Loop                                       /. (expectedOutput /. point), eps];
+           TestClose[expansionUncertainty                           /. output, expansionUncertainty                           /. (expectedOutput /. point), eps];
+           TestClose[deltaLambda3LoopHimalayaDRbarPrime             /. output, deltaLambda3LoopHimalayaDRbarPrime             /. (expectedOutput /. point), eps];
+           TestClose[deltaLambda3LoopHimalayaShiftDRbarPrimeToMSbar /. output, deltaLambda3LoopHimalayaShiftDRbarPrimeToMSbar /. (expectedOutput /. point), eps];
+           TestClose[deltaLambda3LoopHimalayaUncertainty            /. output, deltaLambda3LoopHimalayaUncertainty            /. (expectedOutput /. point), eps];
+           TestClose[deltaLambda3LoopEFTDRbarPrime                  /. output, deltaLambda3LoopEFTDRbarPrime                  /. (expectedOutput /. point), eps];
+           TestClose[deltaLambda3LoopEFTShiftDRbarPrimeToMSbar      /. output, deltaLambda3LoopEFTShiftDRbarPrimeToMSbar      /. (expectedOutput /. point), eps];
+           TestClose[deltaLambda3LoopEFTUncertainty                 /. output, deltaLambda3LoopEFTUncertainty                 /. (expectedOutput /. point), eps];
+          ];
+
 (* load LibrayLink *)
 Get[FileNameJoin[{DirectoryName[$InputFileName], "..", "source", "LibraryLink", "Himalaya_LibraryLink.m"}]];
 
 InitializeHimalaya[libPath];
 
+HimalayaInfoMessage[s_]  := Print[s];
+HimalayaErrorMessage[s_] := Print[s];
+
 Off[General::stop];
 
 (* SPS2 *)
-output = HimalayaCalculateDMh3L[
-    settings -> {
-        bottom -> False,
-        verbose -> True
+pointSPS2 = {
+    input -> {
+        settings -> {
+            bottom -> False,
+            verbose -> True
+        },
+        parameters -> {
+            scale -> 1.11090135*^03,
+            mu -> 3.73337018*^02,
+            g3 -> 1.06187116*^00,
+            vd -> 2.51008404*^01,
+            vu -> 2.41869332*^02,
+            mq2 -> { {2.36646981*^06, 0, 0},
+                     {0, 2.36644973*^06, 0},
+                     {0, 0, 1.63230152*^06} },
+            md2 -> { {2.35612778*^06, 0, 0},
+                     {0, 2.35610884*^06, 0},
+                     {0, 0, 2.31917415*^06} },
+            mu2 -> { {2.35685097*^06, 0, 0},
+                     {0, 2.35682945*^06, 0},
+                     {0, 0, 9.05923409*^05} },
+            Ab -> -784.3356416708631,
+            At -> -527.8746242245387,
+            MA -> 1.48446235*^03,
+            MG -> 6.69045022*^02,
+            MW -> 8.04001915*^01,
+            MZ -> 8.97608307*^01,
+            Mt -> 1.47685846*^02,
+            Mb -> 2.38918959*^00,
+            MSt -> { 9.57566721*^02, 1.28878643*^03 },
+            MSb -> { 1.27884964*^03, 1.52314587*^03 },
+            s2t -> Sin[2*ArcSin[1.13197339*^-01]],
+            s2b -> Sin[2*ArcSin[-9.99883015*^-01]]
+        }
     },
-    parameters -> {
-        scale -> 1.11090135*^03,
-        mu -> 3.73337018*^02,
-        g3 -> 1.06187116*^00,
-        vd -> 2.51008404*^01,
-        vu -> 2.41869332*^02,
-        mq2 -> { {2.36646981*^06, 0, 0},
-                 {0, 2.36644973*^06, 0},
-                 {0, 0, 1.63230152*^06} },
-        md2 -> { {2.35612778*^06, 0, 0},
-                 {0, 2.35610884*^06, 0},
-                 {0, 0, 2.31917415*^06} },
-        mu2 -> { {2.35685097*^06, 0, 0},
-                 {0, 2.35682945*^06, 0},
-                 {0, 0, 9.05923409*^05} },
-        Ab -> -784.3356416708631,
-        At -> -527.8746242245387,
-        MA -> 1.48446235*^03,
-        MG -> 6.69045022*^02,
-        MW -> 8.04001915*^01,
-        MZ -> 8.97608307*^01,
-        Mt -> 1.47685846*^02,
-        Mb -> 2.38918959*^00,
-        MSt -> { 9.57566721*^02, 1.28878643*^03 },
-        MSb -> { 1.27884964*^03, 1.52314587*^03 },
-        s2t -> Sin[2*ArcSin[1.13197339*^-01]],
-        s2b -> Sin[2*ArcSin[-9.99883015*^-01]]
-    }
-];
+    expectedOutput -> {
+        renormalizationScheme                          -> DRbarPrime,
+        hierarchyID                                    -> 2,
+        hierarchyName                                  -> h3q22g,
+        Mstop                                          -> {957.567, 1288.79},
+        Mh2Tree                                        -> {{2.18023*^06, -227080}, {-227080, 31451.3}},
+        Mh21Loop                                       -> {{-2.92153, 100.252}, {100.252, 5277.06}},
+        Mh22Loop                                       -> {{-0.307806, 17.7881}, {17.7881, 1256.76}},
+        Mh23Loop                                       -> {{-0.0213027, 5.63268}, {5.63268, 299.769}},
+        expansionUncertainty                           -> {0, 0, 0.0378711, 0.0121494},
+        deltaLambda3LoopHimalayaDRbarPrime             -> -2.3901*^-05,
+        deltaLambda3LoopHimalayaShiftDRbarPrimeToMSbar -> -4.62215*^-05,
+        deltaLambda3LoopHimalayaUncertainty            -> 1.85817*^-05,
+        deltaLambda3LoopEFTDRbarPrime                  -> -4.04954*^-05,
+        deltaLambda3LoopEFTShiftDRbarPrimeToMSbar      -> -4.62254*^-05,
+        deltaLambda3LoopEFTUncertainty                 -> 1.85778*^-05
+    },
+    precision -> 1*^-5
+};
 
-eps = 10*^-6;
-
-TestEqual[renormalizationScheme /. output, DRbarPrime, eps];
-TestEqual[hierarchyID           /. output, 2, eps];
-TestEqual[hierarchyName         /. output, h3q22g, eps];
-TestClose[Mstop                 /. output, {957.567, 1288.79}, eps];
-TestClose[Mh2Tree               /. output, {{2.18023*^06, -227080}, {-227080, 31451.3}}, eps];
-TestClose[Mh21Loop              /. output, {{-2.92153, 100.252}, {100.252, 5277.06}}, eps];
-TestClose[Mh22Loop              /. output, {{-0.307806, 17.7881}, {17.7881, 1256.76}}, eps];
-TestClose[Mh23Loop              /. output, {{-0.0213027, 5.63268}, {5.63268, 299.769}}, eps];
-TestClose[expansionUncertainty  /. output, {0, 0, 0.0378711, 0.0121494}, eps];
-TestClose[deltaLambda3LoopHimalayaDRbarPrime             /. output, -2.3901*^-05, eps];
-TestClose[deltaLambda3LoopHimalayaShiftDRbarPrimeToMSbar /. output, -4.62215*^-05, eps];
-TestClose[deltaLambda3LoopHimalayaUncertainty            /. output, 1.85817*^-05, eps];
-TestClose[deltaLambda3LoopEFTDRbarPrime                  /. output, -4.04954*^-05, eps];
-TestClose[deltaLambda3LoopEFTShiftDRbarPrimeToMSbar      /. output, -4.62254*^-05, eps];
-TestClose[deltaLambda3LoopEFTUncertainty                 /. output, 1.85778*^-05, eps];
+TestPoint[pointSPS2, "SPS2"];
 
 Print["Number of passed tests: ", passedTests];
 Print["Number of failed tests: ", failedTests];
