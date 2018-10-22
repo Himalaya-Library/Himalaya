86ac408cfbcb85fe49bd90951c423e75918391ba
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-10-22 09:45:53 +0200

    update / add comments

--- source/include/mh2_fo/pv.hpp
+++ source/include/mh2_fo/pv.hpp
@@ -17,8 +17,11 @@
 namespace himalaya {
 namespace mh2_fo {
 
+/// A0 Passarino-Veltman function
 double a0(double m2, double q2) noexcept;
+/// B0 Passarino-Veltman function
 double b0(double p2, double m12, double m22, double q2) noexcept;
+/// derivative of B0 Passarino-Veltman function w.r.t. p^2, for p^2 = 0
 double d1_b0(double m1, double m2) noexcept;
 
 } // namespace mh2_fo
