fc3e6e519a23d01f53fb24436d94be5e4e001f7b
Alexander Voigt <ahgvoigt@gmail.com>
2019-07-29 12:40:26 +0200

    fix indentation

--- source/include/Himalaya_interface.hpp
+++ source/include/Himalaya_interface.hpp
@@ -33,35 +33,35 @@ const double NaN = std::numeric_limits<double>::quiet_NaN();
  */
 struct Parameters {
    // DR'-bar parameters
-   double scale{};                  ///< renormalization scale
-   double mu{};                          ///< mu parameter, convention of [Phys.Rept. 117 (1985) 75-263]
-   double g1{};                          ///< GUT-normalized gauge coupling g1, with gY = g1*Sqrt[3/5]
-   double g2{};                          ///< gauge coupling g2
-   double g3{};                          ///< gauge coupling g3 SU(3)
-   double vd{};                          ///< VEV of down Higgs, with v = Sqrt[vu^2 + vd^2] ~ 246 GeV
-   double vu{};                          ///< VEV of up Higgs, with v = Sqrt[vu^2 + vd^2] ~ 246 GeV
-   RM33 mq2{RM33::Zero()};          ///< soft-breaking squared left-handed squark mass parameters
-   RM33 md2{RM33::Zero()};          ///< soft-breaking squared right-handed down-squark mass parameters
-   RM33 mu2{RM33::Zero()};          ///< soft-breaking squared right-handed up-squark mass parameters
-   RM33 ml2{RM33::Zero()};          ///< soft-breaking squared left-handed slepton mass parameters
-   RM33 me2{RM33::Zero()};          ///< soft-breaking squared right-handed slepton mass parameters
-   RM33 Au{RM33::Zero()};          ///< trilinear up type squark-Higgs coupling matrix
-   RM33 Ad{RM33::Zero()};          ///< trilinear down type squark-Higgs coupling matrix
-   RM33 Ae{RM33::Zero()};          ///< trilinear electron type squark-Higgs coupling matrix
-   RM33 Yu{RM33::Zero()};          ///< up-type yukawa coupling matrix
-   RM33 Yd{RM33::Zero()};          ///< down-type yukawa coupling matrix
-   RM33 Ye{RM33::Zero()};          ///< electron-type yukawa coupling matrix
+   double scale{};                ///< renormalization scale
+   double mu{};                   ///< mu parameter, convention of [Phys.Rept. 117 (1985) 75-263]
+   double g1{};                   ///< GUT-normalized gauge coupling g1, with gY = g1*Sqrt[3/5]
+   double g2{};                   ///< gauge coupling g2
+   double g3{};                   ///< gauge coupling g3 SU(3)
+   double vd{};                   ///< VEV of down Higgs, with v = Sqrt[vu^2 + vd^2] ~ 246 GeV
+   double vu{};                   ///< VEV of up Higgs, with v = Sqrt[vu^2 + vd^2] ~ 246 GeV
+   RM33 mq2{RM33::Zero()};        ///< soft-breaking squared left-handed squark mass parameters
+   RM33 md2{RM33::Zero()};        ///< soft-breaking squared right-handed down-squark mass parameters
+   RM33 mu2{RM33::Zero()};        ///< soft-breaking squared right-handed up-squark mass parameters
+   RM33 ml2{RM33::Zero()};        ///< soft-breaking squared left-handed slepton mass parameters
+   RM33 me2{RM33::Zero()};        ///< soft-breaking squared right-handed slepton mass parameters
+   RM33 Au{RM33::Zero()};         ///< trilinear up type squark-Higgs coupling matrix
+   RM33 Ad{RM33::Zero()};         ///< trilinear down type squark-Higgs coupling matrix
+   RM33 Ae{RM33::Zero()};         ///< trilinear electron type squark-Higgs coupling matrix
+   RM33 Yu{RM33::Zero()};         ///< up-type yukawa coupling matrix
+   RM33 Yd{RM33::Zero()};         ///< down-type yukawa coupling matrix
+   RM33 Ye{RM33::Zero()};         ///< electron-type yukawa coupling matrix
 
    // DR'-bar masses
-   double M1{};                          ///< bino
-   double M2{};                          ///< wino
-   double MG{};                          ///< gluino
+   double M1{};                   ///< bino
+   double M2{};                   ///< wino
+   double MG{};                   ///< gluino
    double MW{NaN};                ///< W
-   double MZ{NaN};                  ///< Z
+   double MZ{NaN};                ///< Z
    double Mt{NaN};                ///< top-quark
    double Mb{NaN};                ///< down-quark
-   double Mtau{NaN};                  ///< tau lepton
-   double MA{};                          ///< CP-odd Higgs
+   double Mtau{NaN};              ///< tau lepton
+   double MA{};                   ///< CP-odd Higgs
    V2 MSt{NaN, NaN};              ///< stops
    V2 MSb{NaN, NaN};              ///< sbottoms
    V2 MStau{NaN, NaN};            ///< staus
@@ -74,7 +74,7 @@ struct Parameters {
    double theta_b{NaN};           ///< sbottom mixing angle
    double theta_tau{NaN};         ///< stau mixing angle
 
-   int massLimit3LThreshold{};           ///< an integer flag to set the mass limit
+   int massLimit3LThreshold{};    ///< an integer flag to set the mass limit
 
    double calculateMsq2() const;  ///< calculates average light squark mass squared
    void validate(bool verbose);   ///< validates the parameter set
