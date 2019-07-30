0d9eb0ee603e0ac8de2d4b4b2888f7235141d15e
Alexander Voigt <ahgvoigt@gmail.com>
2019-07-30 14:09:13 +0200

    remove unused stuff from the example.cpp

--- TODO.md
+++ TODO.md
@@ -2,12 +2,6 @@
 
 ## Interfaces
 
-* Our `example.cpp` should only include the one Himalaya header:
-
-      #include "HierarchyCalculator.hpp"
-
-  This should fixed when the decision on the C++ interface is made.
-
 * update references for 2-loop corrections in the output `operator<<`
   of the `HierarchyCalculator`.  See the references in `README.md`.
 
--- source/example.cpp
+++ source/example.cpp
@@ -6,9 +6,8 @@
 // ====================================================================
 
 #include "HierarchyCalculator.hpp"
-#include "Mh2EFTCalculator.hpp"
-#include "MSSM_mass_eigenstates.hpp"
-#include  <iostream>
+#include <iostream>
+#include <cmath>
 
 himalaya::Parameters setup_point(double MS, double tb, double xt)
 {
@@ -61,64 +60,17 @@ himalaya::Parameters setup_point(double MS, double tb, double xt)
    return pars;
 }
 
-himalaya::Parameters make_gaugeless(const himalaya::Parameters& pars)
-{
-   auto gl = pars;
-   gl.g1 = 0.;
-   gl.g2 = 0.;
-   gl.MW = himalaya::NaN;
-   gl.MZ = himalaya::NaN;
-   gl.validate(false);
-   return gl;
-}
-
 int main()
 {
-   const std::vector<himalaya::Parameters> points = {
-      setup_point(2000., 20., std::sqrt(6.))
-   };
+   const auto point = setup_point(2000., 20., std::sqrt(6.));
+   himalaya::HierarchyCalculator hc(point);
 
-   for (const auto& point: points) {
-      try {
-         // init hierarchy calculator
-         himalaya::HierarchyCalculator hc(point, 0);
-
-         // calculate the 3-loop corrections O(α_t*α_s^2)
-         const auto hoTop = hc.calculateDMh3L(false);
-         std::cout << hoTop;
-
-         // calculate the 3-loop corrections O(α_b*α_s^2)
-         //himalaya::HierarchyObject hoBot = hc.calculateDMh3L(true);
-
-         /*const auto point_gl = make_gaugeless(point);
-
-         // calculate fixed-order corrections for v^2 << MS^2
-         himalaya::mh2_eft::Mh2EFTCalculator meft(point_gl);
-         //himalaya::mh2_eft::Mh2EFTCalculator meft(point);
-         const auto dmh2_eft_0l = meft.getDeltaMh2EFT0Loop();
-         const auto dmh2_eft_1l = meft.getDeltaMh2EFT1Loop(1,1);
-         const auto dmh2_eft_2l = meft.getDeltaMh2EFT2Loop(1,1);
-
-         std::cout << "Mh^2_EFT_0L  = " << dmh2_eft_0l << " GeV^2 O(g1^2, g2^2)\n";
-         std::cout << "ΔMh^2_EFT_1L = " << dmh2_eft_1l << " GeV^2 O(αt + αb + ατ)\n";
-         std::cout << "ΔMh^2_EFT_2L = " << dmh2_eft_2l
-                   << " GeV^2 O((αt+ab)*αs + (αt+αb)^2 + ab*aτ + aτ^2)\n";
-
-         // calculate fixed-order corrections
-         himalaya::mh2_fo::MSSM_mass_eigenstates mfo(point_gl, true);
-         //himalaya::mh2_fo::MSSM_mass_eigenstates mfo(point);
-         const auto dmh_fo     = mfo.calculate_Mh2();
-         const auto dmh2_fo_0l = std::get<0>(dmh_fo);
-         const auto dmh2_fo_1l = std::get<1>(dmh_fo);
-         const auto dmh2_fo_2l = std::get<2>(dmh_fo);
-
-         std::cout << "Mh^2_FO_0L   = " << dmh2_fo_0l << " GeV^2 O(full)\n";
-         std::cout << "ΔMh^2_FO_1L  = " << dmh2_fo_1l << " GeV^2 O(αt + αb + ατ)\n";
-         std::cout << "ΔMh^2_FO_2L  = " << dmh2_fo_2l
-                   << " GeV^2 O((αt+ab)*αs + (αt+αb)^2 + ab*aτ + aτ^2)\n";*/
-      } catch (const std::exception& e) {
-         std::cerr << "Error: " << e.what() << '\n';
-      }
+   try {
+      // calculate the 3-loop corrections O(α_t*α_s^2)
+      const auto ho = hc.calculateDMh3L(false);
+      std::cout << ho;
+   } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
    }
 
    return 0;
