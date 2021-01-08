dc3d184fdcf5c1a6558b6d6806db8d9f164c1ad5
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-08 10:19:42 +0100

    implement numeric values for numeric constants

--- source/include/hierarchies/Constants.hpp
+++ source/include/hierarchies/Constants.hpp
@@ -13,16 +13,19 @@
 namespace himalaya {
 namespace {
 
-const double Pi = 3.141592653589793; ///< Pi
-const double z2 = pow2(Pi) / 6.;     ///< Zeta[2]
-const double z3 = 1.202056903159594; ///< Zeta[3]
-const double z4 = pow4(Pi) / 90.;    ///< Zeta[4]
-const double sqrt2 = std::sqrt(2.);  ///< Sqrt[2]
-const double sqrt3 = std::sqrt(3.);  ///< Sqrt[3]
-const double log2 = std::log(2.);    ///< Log[2]
-const double log3 = std::log(3.);    ///< Log[3]
+const double Pi        = 3.14159265358979324;      ///< Pi
+const double z2        = 1.64493406684822644;      ///< Zeta[2] = Pi^2/6
+const double z3        = 1.20205690315959429;      ///< Zeta[3]
+const double z4        = 1.08232323371113819;      ///< Zeta[4] = Pi^4/90
+const double sqrt2     = 1.41421356237309505;      ///< Sqrt[2]
+const double sqrt3     = 1.73205080756887729;      ///< Sqrt[3]
+const double log2      = 0.693147180559945309;     ///< Log[2]
+const double log3      = 1.09861228866810969;      ///< Log[3]
+const double oneLoop   = 6.332573977646110963e-03; ///< 1/(4 Pi)^2
+const double twoLoop   = 4.010149318236068752e-05; ///< 1/(4 Pi)^4
+const double threeLoop = 2.539456721913701978e-07; ///< 1/(4 Pi)^6
 
-// poly logs
+// polylogs
 const double pl412 = 0.51747906167389934317668576113647; ///< PolyLog[4,1/2]
 const std::complex<double> pl2expPi3(0.27415567780803773941206919444, 1.014941606409653625021202554275); ///< PolyLog[2, Exp[I Pi / 3]]
 const std::complex<double> pl3expPi6sqrt3(0.51928806536375962552715984277228, - 0.33358157526196370641686908633664); ///< PolyLog[3, Exp[- I Pi / 6] / Sqrt[3]]
