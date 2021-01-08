fd66827525f2d7dc0072643b3545d48990ccb918
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-08 17:06:44 +0100

    distribute MS randomly

--- test/test_benchmark.cpp
+++ test/test_benchmark.cpp
@@ -42,36 +42,39 @@ auto rTB = [] { return random(2, 100); };
 auto rXT = [] { return random(-3.0, 3.0); };
 
 
+/// random number for variation of MS
+auto rVar = [] { return random(0.9, 1.1); };
+
+
 /// generate random parameter point of FO calculation
 himalaya::Parameters setup_point(double MS, double tb, double xt)
 {
    himalaya::Parameters pars;
 
-   const double MS2 = MS*MS;
    const double Xt = xt*MS;
    const double beta = std::atan(tb);
-   pars.scale = MS;
-   pars.mu = MS;
+   pars.scale = MS*rVar();
+   pars.mu = MS*rVar();
    pars.g1 = 0.46;
    pars.g2 = 0.65;
    pars.g3 = 1.166;
    pars.vd = 246*std::cos(beta);
    pars.vu = 246*std::sin(beta);
-   pars.mq2 << MS2, 0, 0,
-               0, MS2, 0,
-               0, 0, MS2;
-   pars.md2 << MS2, 0, 0,
-               0, MS2, 0,
-               0, 0, MS2;
-   pars.mu2 << MS2, 0, 0,
-               0, MS2, 0,
-               0, 0, MS2;
-   pars.ml2 << MS2, 0, 0,
-               0, MS2, 0,
-               0, 0, MS2;
-   pars.me2 << MS2, 0, 0,
-               0, MS2, 0,
-               0, 0, MS2;
+   pars.mq2 << sqr(MS*rVar()), 0, 0,
+               0, sqr(MS*rVar()), 0,
+               0, 0, sqr(MS*rVar());
+   pars.md2 << sqr(MS*rVar()), 0, 0,
+               0, sqr(MS*rVar()), 0,
+               0, 0, sqr(MS*rVar());
+   pars.mu2 << sqr(MS*rVar()), 0, 0,
+               0, sqr(MS*rVar()), 0,
+               0, 0, sqr(MS*rVar());
+   pars.ml2 << sqr(MS*rVar()), 0, 0,
+               0, sqr(MS*rVar()), 0,
+               0, 0, sqr(MS*rVar());
+   pars.me2 << sqr(MS*rVar()), 0, 0,
+               0, sqr(MS*rVar()), 0,
+               0, 0, sqr(MS*rVar());
    pars.Au << 0, 0, 0,
               0, 0, 0,
               0, 0, Xt + pars.mu/tb;
@@ -80,10 +83,10 @@ himalaya::Parameters setup_point(double MS, double tb, double xt)
    pars.Yu << 0, 0, 0, 0, 0, 0, 0, 0, 0.862;
    pars.Yd << 0, 0, 0, 0 ,0 ,0 ,0 ,0, 0.133;
    pars.Ye << 0, 0, 0, 0, 0, 0, 0, 0, 0.101;
-   pars.MA = MS;
-   pars.M1 = MS;
-   pars.M2 = MS;
-   pars.MG = MS;
+   pars.MA = MS*rVar();
+   pars.M1 = MS*rVar();
+   pars.M2 = MS*rVar();
+   pars.MG = MS*rVar();
 
    pars.validate(verbose);
 
