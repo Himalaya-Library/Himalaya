bfb8035794218f72dd6778e07c79f6cbe67bab09
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-10 17:41:38 +0100

    re-use power functions (follow DRY)

--- source/include/hierarchies/Utils.hpp
+++ source/include/hierarchies/Utils.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include "power.hpp"
 #include <complex>
 
 namespace himalaya {
@@ -43,19 +44,5 @@ operator- ( const std::complex<T>& c, SCALAR n ) noexcept { return c - T(n) ; }
 template< typename T, typename SCALAR > inline
 typename std::enable_if< !std::is_same<T,SCALAR>::value, std::complex<T> >::type
 operator- ( SCALAR n, const std::complex<T>& c ) noexcept { return T(n) - c ; }
-
-template <typename T> T pow2(T x)    noexcept { return x*x; }
-template <typename T> T pow3(T x)    noexcept { return x*x*x; }
-template <typename T> T pow4(T x)    noexcept { return x*x*x*x; }
-template <typename T> T pow5(T x)    noexcept { return x*x*x*x*x; }
-template <typename T> T pow6(T x)    noexcept { return x*x*x*x*x*x; }
-template <typename T> T pow7(T x)    noexcept { return x*x*x*x*x*x*x; }
-template <typename T> T pow8(T x)    noexcept { return x*x*x*x*x*x*x*x; }
-template <typename T> T pow9(T x)    noexcept { return x*x*x*x*x*x*x*x*x; }
-template <typename T> T power10(T x) noexcept { return x*x*x*x*x*x*x*x*x*x; }
-template <typename T> T pow11(T x)   noexcept { return x*x*x*x*x*x*x*x*x*x*x; }
-template <typename T> T pow12(T x)   noexcept { return x*x*x*x*x*x*x*x*x*x*x*x; }
-template <typename T> T pow13(T x)   noexcept { return x*x*x*x*x*x*x*x*x*x*x*x*x; }
-template <typename T> T pow14(T x)   noexcept { return x*x*x*x*x*x*x*x*x*x*x*x*x*x; }
 
 } // namespace himalaya
