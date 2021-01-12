538d5b8bfd644c5353077494ead1a844b6d47c43
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-12 10:12:34 +0100

    remove redundant enum initializers and unused enum

--- source/include/hierarchies/enums.hpp
+++ source/include/hierarchies/enums.hpp
@@ -38,36 +38,22 @@ namespace ExpansionDepth {
 
 /// expansion depth flags
 enum ExpansionDepth : int {
-   FIRST     =  0,
-   threeLoop =  FIRST,   ///< truncate the two loop expansion at the three loop expansion depth
-   Mst       =  1,       ///< truncate the expansion depth in the stop/sbottom masses by one order
-   Dmglst1   =  2,       ///< truncate the expansion depth in the difference of stop/sbottom 1 mass and the gluino mass by one order
-   Dmsqst1   =  3,       ///< truncate the expansion depth in the difference of the stop/sbottom 1 mass and the average squark mass by one order
-   Dmst12    =  4,       ///< truncate the expansion depth in the difference of the stop/sbottom masses by one order
-   At        =  5,       ///< truncate the expansion depth in At/Ab by one order
-   lmMsusy   =  6,       ///< truncate the expansion depth in log(Msusy) by one order
-   Msq       =  7,       ///< truncate the expansion depth in the average squark mass by one order
-   Msusy     =  8,       ///< truncate the expansion depth in the average SUSY mass by one order
-   Dmglst2   =  9,       ///< truncate the expansion depth in the difference of the stop/sbottom 2 mass and the gluino mass by one order
-   Dmsqst2   = 10,       ///< truncate the expansion depth in the difference of the average squark mass and the stop/sbottom 2 mass by one order
-   Mgl       = 11,       ///< truncate the expansion depth in the gluino mass by one order
+   threeLoop,       ///< truncate the two loop expansion at the three loop expansion depth
+   Mst,             ///< truncate the expansion depth in the stop/sbottom masses by one order
+   Dmglst1,         ///< truncate the expansion depth in the difference of stop/sbottom 1 mass and the gluino mass by one order
+   Dmsqst1,         ///< truncate the expansion depth in the difference of the stop/sbottom 1 mass and the average squark mass by one order
+   Dmst12,          ///< truncate the expansion depth in the difference of the stop/sbottom masses by one order
+   At,              ///< truncate the expansion depth in At/Ab by one order
+   lmMsusy,         ///< truncate the expansion depth in log(Msusy) by one order
+   Msq,             ///< truncate the expansion depth in the average squark mass by one order
+   Msusy,           ///< truncate the expansion depth in the average SUSY mass by one order
+   Dmglst2,         ///< truncate the expansion depth in the difference of the stop/sbottom 2 mass and the gluino mass by one order
+   Dmsqst2,         ///< truncate the expansion depth in the difference of the average squark mass and the stop/sbottom 2 mass by one order
+   Mgl,             ///< truncate the expansion depth in the gluino mass by one order
    NUMBER_OF_EXPANSIONS
 };
 
 } // namespace ExpansionDepth
-
-/// Mass schemes
-namespace MassSchemes {
-
-   /// mass scheme flags
-   enum MassSchemes : int {
-      FIRST        = 0,
-      DEFAULT      = FIRST,
-      MASSEIGEN    = 1,
-      SOFTBREAKING = 2,
-      NUMBER_OF_SCHEMES
-   };
-}
 
 } // namespace hierarchies
 } // namespace himalaya
