8b747b0d2039d3fae99c3bb6aa2dc92e11622397
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-13 06:52:27 +0100

    adding header that includes all hierarchies

---
+++ source/include/hierarchies/Hierarchies.hpp
@@ -0,0 +1,23 @@
+// ====================================================================
+// This file is part of Himalaya.
+//
+// Himalaya is licenced under the GNU General Public License (GNU GPL)
+// version 3.
+// ====================================================================
+
+#pragma once
+
+#include "H3.hpp"
+#include "H32q2g.hpp"
+#include "H3q22g.hpp"
+#include "H4.hpp"
+#include "H5.hpp"
+#include "H5g1.hpp"
+#include "H6.hpp"
+#include "H6b.hpp"
+#include "H6b2qg2.hpp"
+#include "H6bq22g.hpp"
+#include "H6bq2g2.hpp"
+#include "H6g2.hpp"
+#include "H9.hpp"
+#include "H9q2.hpp"
