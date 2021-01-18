35dfcf38a489983863566306be4ab6215a2fc12c
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-18 10:31:05 +0100

    rename file

--- source/mh2l/Li2f.f90
+++ source/mh2l/Li2f.f90
@@ -1,9 +1,6 @@
 !>    @brief C wrapper for complex dilogarithm
-      subroutine li2c(re_in, im_in, re_out, im_out)
-     $ bind(C, name="li2c_")
-
+      subroutine li2c(re_in, im_in, re_out, im_out) bind(C, name="li2c_")
       implicit none
-
       double precision, intent(in) :: re_in, im_in
       double precision, intent(out) :: re_out, im_out
       double complex z, l, cdli2
