e5c8faa36f1c6b48bfec853b624657cd04b674ef
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-19 21:10:28 +0100

    make power functions constexpr

--- source/mh2_eft/threshold_loop_functions.cpp
+++ source/mh2_eft/threshold_loop_functions.cpp
@@ -33,14 +33,14 @@
 namespace himalaya {
 namespace threshold_loop_functions {
 namespace {
-   template <typename T> T sqr(T x) noexcept { return x*x; }
-   template <typename T> T cube(T x) noexcept { return x*x*x; }
-   template <typename T> T quad(T x) noexcept { return x*x*x*x; }
-   template <typename T> T pow5(T x) noexcept { return x*x*x*x*x; }
-   template <typename T> T pow6(T x) noexcept { return x*x*x*x*x*x; }
-   template <typename T> T pow7(T x) noexcept { return x*x*x*x*x*x*x; }
-   template <typename T> T pow8(T x) noexcept { return x*x*x*x*x*x*x*x; }
-   template <typename T> T pow9(T x) noexcept { return x*x*x*x*x*x*x*x*x; }
+   template <typename T> constexpr T  sqr(T x) noexcept { return x*x; }
+   template <typename T> constexpr T cube(T x) noexcept { return x*x*x; }
+   template <typename T> constexpr T quad(T x) noexcept { return sqr(sqr(x)); }
+   template <typename T> constexpr T pow5(T x) noexcept { return quad(x) * x; }
+   template <typename T> constexpr T pow6(T x) noexcept { return sqr(sqr(x) * x); }
+   template <typename T> constexpr T pow7(T x) noexcept { return pow6(x) * x; }
+   template <typename T> constexpr T pow8(T x) noexcept { return sqr(quad(x)); }
+   template <typename T> constexpr T pow9(T x) noexcept { return pow8(x) * x; }
 
    template <typename T>
    bool is_zero(T a, T prec) noexcept
