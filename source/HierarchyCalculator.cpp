163d5669413bd620621273eef4b16ed959c905be
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-20 18:59:59 +0100

    make constructor noexcept

--- source/HierarchyCalculator.cpp
+++ source/HierarchyCalculator.cpp
@@ -47,15 +47,17 @@ namespace {
 template <class T>
 class RAII_tmp_set {
 public:
-   RAII_tmp_set(T& variable_, T new_value_)
+   RAII_tmp_set(T& variable_, T new_value_) noexcept
       : variable(variable_)
       , old_value(variable_)
       {
          variable_ = new_value_;
       }
    RAII_tmp_set(const RAII_tmp_set&) = delete;
-   RAII_tmp_set(RAII_tmp_set&&) = delete;
+   RAII_tmp_set(RAII_tmp_set&&) noexcept = delete;
    ~RAII_tmp_set() { variable = old_value; }
+   RAII_tmp_set& operator=(const RAII_tmp_set&) = delete;
+   RAII_tmp_set& operator=(RAII_tmp_set&& other) noexcept = default;
 private:
    T& variable;
    T old_value;
