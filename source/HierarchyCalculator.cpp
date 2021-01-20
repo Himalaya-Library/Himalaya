1ef211a430132d5f9c12e076950a5985538202e7
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-20 18:48:44 +0100

    consistent indentation

--- source/HierarchyCalculator.cpp
+++ source/HierarchyCalculator.cpp
@@ -44,21 +44,21 @@ namespace {
 
 /// temporarily sets a variable to a new value, and resets the value
 /// to the old one when destoyed
-template<class T>
+template <class T>
 class RAII_tmp_set {
 public:
-  RAII_tmp_set(T& variable_, T new_value_)
-    : variable(variable_)
-    , old_value(variable_)
-  {
-    variable_ = new_value_;
-  }
-  RAII_tmp_set(const RAII_tmp_set&) = delete;
-  RAII_tmp_set(RAII_tmp_set&&) = delete;
-  ~RAII_tmp_set() { variable = old_value; }
+   RAII_tmp_set(T& variable_, T new_value_)
+      : variable(variable_)
+      , old_value(variable_)
+      {
+         variable_ = new_value_;
+      }
+   RAII_tmp_set(const RAII_tmp_set&) = delete;
+   RAII_tmp_set(RAII_tmp_set&&) = delete;
+   ~RAII_tmp_set() { variable = old_value; }
 private:
-  T& variable;
-  T old_value;
+   T& variable;
+   T old_value;
 };
 
 /**
