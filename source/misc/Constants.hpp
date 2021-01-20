c4f746b93c839b1addc02b6357846f2041f89b81
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-20 18:46:11 +0100

    add Mathematica expressions for constants

--- source/misc/Constants.hpp
+++ source/misc/Constants.hpp
@@ -23,26 +23,15 @@ const double sqrt15    = 3.87298334620741689;      ///< Sqrt[15]
 const double sqrt35    = 0.774596669241483377;     ///< Sqrt[3/5]
 const double inv_sqrt2 = 0.707106781186547524;     ///< 1/Sqrt[2]
 const double log2      = 0.693147180559945309;     ///< Log[2]
-const double log3      = 1.09861228866810969;      ///< Log[3]
 const double oneLoop   = 6.332573977646110963e-03; ///< 1/(4 Pi)^2
 const double twoLoop   = 4.010149318236068752e-05; ///< 1/(4 Pi)^4
 const double threeLoop = 2.539456721913701978e-07; ///< 1/(4 Pi)^6
-
-// polylogs
-const double pl412 = 0.51747906167389934317668576113647; ///< PolyLog[4,1/2]
-const std::complex<double> pl2expPi3(0.27415567780803773941206919444, 1.014941606409653625021202554275); ///< PolyLog[2, Exp[I Pi / 3]]
-const std::complex<double> pl3expPi6sqrt3(0.51928806536375962552715984277228, - 0.33358157526196370641686908633664); ///< PolyLog[3, Exp[- I Pi / 6] / Sqrt[3]]
-
-// polylog functions
-const double B4 = -4 * z2 * pow2(log2) + 2 / 3.* pow4(log2) - 13 / 2. * z4 + 16. * pl412;
-const double D3 = 6 * z3 - 15 / 4. * z4 - 6. * pow2(std::imag(pl2expPi3));
-const double DN = 6 * z3 - 4 * z2 * pow2(log2) + 2 / 3. * pow4(log2) - 21 / 2. * z4 + 16. * pl412;
-const double OepS2 = - 763 / 32. - (9 * Pi * sqrt3 * pow2(log3)) / 16. - (35 * pow3(Pi) * sqrt3) / 48.
-   + 195 / 16. * z2 - 15 / 4. * z3 + 57 / 16. * z4 + 45 * sqrt3 / 2. * std::imag(pl2expPi3)
-   - 27 * sqrt3 * std::imag(pl3expPi6sqrt3);
-const double S2 = 4 * std::imag(pl2expPi3) / (9. * sqrt3);
-const double T1ep = - 45 / 2. - (Pi * sqrt3 * pow2(log3)) / 8. - (35 * pow3(Pi) * sqrt3) / 216. - 9 / 2. * z2 + z3 
-   + 6. * sqrt3 * std::imag(pl2expPi3) - 6. * sqrt3 * std::imag(pl3expPi6sqrt3);
+const double B4        = -1.7628000870737709;      ///< -4 Zeta[2] Log[2]^2 + 2/3 Log[2]^4 - 13/2 Zeta[4] + 16 PolyLog[4,1/2]
+const double D3        = -3.0270094939876520;      ///< 6 Zeta[3] - 15/4 Zeta[4] - 6 Im[PolyLog[2, Exp[I Pi/3]]]^2
+const double DN        =  1.1202483970392421;      ///< 6 Zeta[3] - 4 Zeta[2] Log[2]^2 + 2/3 Log[2]^4 - 21/2 Zeta[4] + 16 PolyLog[4,1/2]
+const double OepS2     =  7.8517428364255312;      ///< -763/32 - 9 Pi Sqrt[3] Log[3]^2/16 - 35 Pi^3 Sqrt[3]/48 + 195/16 Zeta[2] - 15/4 Zeta[3] + 57/16 Zeta[4] + 45 Sqrt[3]/2 Im[PolyLog[2, Exp[I Pi / 3]]] - 27 Sqrt[3] Im[PolyLog[3, Exp[-I Pi/6]/Sqrt[3]]]
+const double S2        = 0.26043413763216210;      ///< 4 Im[PolyLog[2, Exp[I Pi/3]]]/(9 Sqrt[3])
+const double T1ep      = -24.208928021203593;      ///< -45/2 - Pi Sqrt[3] Log[3]^2/8 - 35 Pi^3 Sqrt[3]/216 - 9/2 Zeta[2] + Zeta[3] + 6 Sqrt[3] Im[PolyLog[2, Exp[I Pi/3]]] - 6 Sqrt[3] Im[PolyLog[3, Exp[-I Pi/6]/Sqrt[3]]]
 
 } // anonymous namespace
 } // namespace himalaya
