3a1cc5564757d09cd00d9ba0786cb839d54c6f76
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-30 02:14:06 +0100

    split long file into logical parts

---
+++ source/himalaya/mh2_fo/MSSM_spectrum.hpp
@@ -0,0 +1,122 @@
+// ====================================================================
+// This file is part of Himalaya.
+//
+// Himalaya is licenced under the GNU General Public License (GNU GPL)
+// version 3.
+// ====================================================================
+
+#pragma once
+
+#include "himalaya/Himalaya_interface.hpp"
+
+#include <iosfwd>
+#include <Eigen/Core>
+
+/**
+ * @file MSSM_spectrum.hpp
+ *
+ * @brief Contains the definition of the \a MSSM_spectrum class.
+ */
+
+namespace himalaya {
+namespace mh2_fo {
+
+/**
+ * @class MSSM_spectrum
+ *
+ * @brief Contains the tree-level DR' mass spectrum and mixing
+ * matrices.
+ */
+struct MSSM_spectrum {
+   MSSM_spectrum(const Parameters&);
+
+   using A2 = Eigen::Array<double,2,1>;
+   using A4 = Eigen::Array<double,4,1>;
+
+   Parameters pars;         ///< MSSM DR' parameters
+
+   double M2VWm{0.};        ///< MSSM DR' squared W mass
+   double M2VZ{0.};         ///< MSSM DR' squared Z mass
+   double MFt{0.};          ///< MSSM DR' top mass
+   double MFb{0.};          ///< MSSM DR' bottom mass
+   double MFtau{0.};        ///< MSSM DR' tau mass
+   double M2SveL{0.};       ///< MSSM DR' electron-like sneutrino mass
+   double M2SvmL{0.};       ///< MSSM DR' muon-like sneutrino mass
+   double M2SvtL{0.};       ///< MSSM DR' tau-like sneutrino mass
+   A2 M2Su{V2::Zero()};     ///< MSSM DR' squared sup masses
+   A2 M2Sd{V2::Zero()};     ///< MSSM DR' squared sdown masses
+   A2 M2Sc{V2::Zero()};     ///< MSSM DR' squared scharm masses
+   A2 M2Ss{V2::Zero()};     ///< MSSM DR' squared sstrange masses
+   A2 M2St{V2::Zero()};     ///< MSSM DR' squared stop masses
+   A2 M2Sb{V2::Zero()};     ///< MSSM DR' squared sbottom masses
+   A2 M2Se{V2::Zero()};     ///< MSSM DR' squared selectron masses
+   A2 M2Sm{V2::Zero()};     ///< MSSM DR' squared smuon masses
+   A2 M2Stau{V2::Zero()};   ///< MSSM DR' squared stau masses
+   A2 M2hh{A2::Zero()};     ///< MSSM DR' CP-even higgs mass
+   A2 M2Ah{A2::Zero()};     ///< MSSM DR' CP-odd higgs mass
+   A2 M2Hpm{A2::Zero()};    ///< MSSM DR' charged higgs mass
+   A4 MChi{A4::Zero()};     ///< MSSM DR' neutralino mass
+   A2 MCha{A2::Zero()};     ///< MSSM DR' chargino mass
+
+   RM22 ZU{RM22::Zero()};   ///< MSSM DR' sup mixing matrix
+   RM22 ZD{RM22::Zero()};   ///< MSSM DR' sdown mixing matrix
+   RM22 ZC{RM22::Zero()};   ///< MSSM DR' scharm mixing matrix
+   RM22 ZS{RM22::Zero()};   ///< MSSM DR' sstrange mixing matrix
+   RM22 ZT{RM22::Zero()};   ///< MSSM DR' stop mixing matrix
+   RM22 ZB{RM22::Zero()};   ///< MSSM DR' sbottom mixing matrix
+   RM22 ZE{RM22::Zero()};   ///< MSSM DR' selectron mixing matrix
+   RM22 ZM{RM22::Zero()};   ///< MSSM DR' smuon mixing matrix
+   RM22 ZTau{RM22::Zero()}; ///< MSSM DR' stau mixing matrix
+   RM22 ZH{RM22::Zero()};   ///< MSSM DR' CP-even Higgs mixing matrix
+   RM22 ZA{RM22::Zero()};   ///< MSSM DR' CP-odd Higgs mixing matrix
+   RM22 ZP{RM22::Zero()};   ///< MSSM DR' charged Higgs mixing matrix
+   RM44 ZN{RM44::Zero()};   ///< MSSM DR' neutralino mixing matrix
+   RM22 UM{RM22::Zero()};   ///< MSSM DR' positive chargino mixing matrix
+   RM22 UP{RM22::Zero()};   ///< MSSM DR' negative chargino mixing matrix
+
+   void calculate_spectrum(); ///< calculates all DR' masses and mixings
+
+   void calculate_MVWm();   ///< calculates DR' W mass
+   void calculate_MVZ();    ///< calculates DR' Z mass
+   void calculate_MFt();    ///< calculates DR' top mass
+   void calculate_MFb();    ///< calculates DR' bottom mass
+   void calculate_MFtau();  ///< calculates DR' tau mass
+   void calculate_MSveL();  ///< calculates DR' electron-like sneutrino mass
+   void calculate_MSvmL();  ///< calculates DR' muon-like sneutrino mass
+   void calculate_MSvtL();  ///< calculates DR' tau-like sneutrino mass
+   void calculate_MSu();    ///< calculates DR' sup masses
+   void calculate_MSd();    ///< calculates DR' sdown masses
+   void calculate_MSc();    ///< calculates DR' scharm masses
+   void calculate_MSs();    ///< calculates DR' sstrange masses
+   void calculate_MSt();    ///< calculates DR' stop masses
+   void calculate_MSb();    ///< calculates DR' sbottom masses
+   void calculate_MSe();    ///< calculates DR' selectron masses
+   void calculate_MSm();    ///< calculates DR' smuon masses
+   void calculate_MStau();  ///< calculates DR' stau masses
+   void calculate_Mhh();    ///< calculates DR' CP-even Higgs masses
+   void calculate_MAh();    ///< calculates DR' CP-odd Higgs masses
+   void calculate_MHpm();   ///< calculates DR' charged Higgs masses
+   void calculate_MChi();   ///< calculates DR' neutralino masses
+   void calculate_MCha();   ///< calculates DR' chargino masses
+
+   RM22 get_mass_matrix_Su() const;   ///< sup mass matrix
+   RM22 get_mass_matrix_Sd() const;   ///< sdown mass matrix
+   RM22 get_mass_matrix_Sc() const;   ///< scharm mass matrix
+   RM22 get_mass_matrix_Ss() const;   ///< sstrange mass matrix
+   RM22 get_mass_matrix_St() const;   ///< stop mass matrix
+   RM22 get_mass_matrix_Sb() const;   ///< sbottom mass matrix
+   RM22 get_mass_matrix_Se() const;   ///< selectron mass matrix
+   RM22 get_mass_matrix_Sm() const;   ///< smuon mass matrix
+   RM22 get_mass_matrix_Stau() const; ///< stau mass matrix
+   RM22 get_mass_matrix_hh() const;   ///< CP-even Higgs mass matrix
+   RM22 get_mass_matrix_Ah() const;   ///< CP-odd Higgs mass matrix
+   RM22 get_mass_matrix_Hpm() const;  ///< charged Higgs mass matrix
+   RM44 get_mass_matrix_Chi() const;  ///< neutralino mass matrix
+   RM22 get_mass_matrix_Cha() const;  ///< chargino mass matrix
+};
+
+/// prints the spectrum
+std::ostream& operator<<(std::ostream&, const MSSM_spectrum&);
+
+} // namespace mh2_fo
+} // namespace himalaya
