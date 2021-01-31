b44cb40bf8536e6892439d02ecc9f09a064251a0
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-31 02:34:28 +0100

    remove unnecessary if branch

--- source/himalaya/mh2_fo/PV.cpp
+++ source/himalaya/mh2_fo/PV.cpp
@@ -109,8 +109,6 @@ double b0xx(double p2, double m2, double q2) noexcept
 
    if (p2 < EPSTOL && m2 < EPSTOL) {
       return 0; // IR divergence
-   } else if (std::abs(p2 - m2) < 1e-13) {
-      return 0.18620063576578215 - std::log(m2 / q2); // 2 - Pi/Sqrt[3]
    } else if (p2 < 1e-15) {
       return -std::log(m2 / q2);
    } else if (p2 <= 4 * m2) {
