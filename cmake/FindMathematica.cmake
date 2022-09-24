58d93118efddbe1a9ad5a88db4580050bc70dbf2
Alexander Voigt <ahgvoigt@gmail.com>
2022-09-24 17:25:03 +0200

    update FindMathematica to version 3.7.0

--- cmake/FindMathematica.cmake
+++ cmake/FindMathematica.cmake
@@ -34,7 +34,7 @@ cmake_minimum_required(VERSION 2.8.12)
 cmake_policy(POP)
 
 set (Mathematica_CMAKE_MODULE_DIR "${CMAKE_CURRENT_LIST_DIR}")
-set (Mathematica_CMAKE_MODULE_VERSION "3.6.0")
+set (Mathematica_CMAKE_MODULE_VERSION "3.7.0")
 
 # activate select policies
 if (POLICY CMP0025)
@@ -248,7 +248,7 @@ macro (_get_program_names _outProgramNames)
 		"gridMathematica Server")
 	# Mathematica product versions in order of preference
 	set (_MathematicaVersions
-		"13.0"
+		"13.1" "13.0"
 		"12.3" "12.2" "12.1" "12.0"
 		"11.3" "11.2" "11.1" "11.0"
 		"10.4" "10.3" "10.2" "10.1" "10.0"
