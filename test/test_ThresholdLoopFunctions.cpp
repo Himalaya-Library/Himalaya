f45f111f4a1392d724fd1d7b3a092a426ad0881e
Alexander Voigt <ahgvoigt@gmail.com>
2024-01-13 15:31:15 +0100

    test further threshold function F*

--- test/test_ThresholdLoopFunctions.cpp
+++ test/test_ThresholdLoopFunctions.cpp
@@ -1,6 +1,5 @@
 #include "doctest.h"
 #include "himalaya/mh2_eft/ThresholdLoopFunctions.hpp"
-#include <algorithm>
 #include <fstream>
 #include <iterator>
 #include <sstream>
@@ -17,16 +16,6 @@ const char PATH_SEPARATOR =
 #else
    '/';
 #endif
-
-
-/// tranform vector of elements of type A -> B
-template <class B, class A, class F>
-std::vector<B> map(F f, const std::vector<A>& in)
-{
-   std::vector<B> out;
-   std::transform(in.cbegin(), in.cend(), std::back_inserter(out), f);
-   return out;
-}
 
 
 /// read data line-wise from file
@@ -47,23 +36,73 @@ std::vector<std::vector<T>> read_data(const std::string& filename)
 }
 
 
-template <class Fn>
-void test_3(const std::string& function_name, Fn fn, double eps)
+std::string create_filename(const std::string& function_name)
 {
-   const auto filename = std::string(TEST_DATA_DIR) + PATH_SEPARATOR + "data" +
-                         PATH_SEPARATOR + function_name + ".dat";
+   return std::string(TEST_DATA_DIR) + PATH_SEPARATOR + "data" +
+                      PATH_SEPARATOR + function_name + ".txt";
+}
 
-   struct Data { double x{}, y{}, z{}, f{}; };
 
-   const auto data = read_data<double>(filename);
+template <class Fn>
+void test_1(const std::string& function_name, Fn fn, double eps)
+{
+   const auto data = read_data<double>(create_filename(function_name));
 
    for (auto d: data) {
-      const double x = d.at(0), y = d.at(1), z = d.at(2), ex = d.at(3);
-      const auto f = fn(x, y, z);
-      INFO("expected: " << ex << ", observed: " << f);
+      const auto ex = d.at(1);
+      const auto f = fn(d.at(0));
+      INFO(function_name << "( " << d.at(0) << " ): expected: " << ex << ", observed: " << f);
       CHECK_CLOSE(ex, f, eps);
    }
 }
+
+
+template <class Fn>
+void test_2(const std::string& function_name, Fn fn, double eps)
+{
+   const auto data = read_data<double>(create_filename(function_name));
+
+   for (auto d: data) {
+      const auto ex = d.at(2);
+      const auto f = fn(d.at(0), d.at(1));
+      INFO(function_name << "( " << d.at(0) << ", " << d.at(1) << "): expected: " << ex << ", observed: " << f);
+      CHECK_CLOSE(ex, f, eps);
+   }
+}
+
+
+template <class Fn>
+void test_3(const std::string& function_name, Fn fn, double eps)
+{
+   const auto data = read_data<double>(create_filename(function_name));
+
+   for (auto d: data) {
+      const auto ex = d.at(3);
+      const auto f = fn(d.at(0), d.at(1), d.at(2));
+      INFO(function_name << "( " << d.at(0) << ", " << d.at(1) << ", " << d.at(2) << "): expected: " << ex << ", observed: " << f);
+      CHECK_CLOSE(ex, f, eps);
+   }
+}
+
+
+TEST_CASE("test_1")
+{
+   test_1("F1", himalaya::mh2_eft::threshold_loop_functions::F1, 1e-11);
+   test_1("F2", himalaya::mh2_eft::threshold_loop_functions::F2, 1e-11);
+   test_1("F3", himalaya::mh2_eft::threshold_loop_functions::F3, 1e-11);
+   test_1("F4", himalaya::mh2_eft::threshold_loop_functions::F4, 1e-11);
+   test_1("F5", himalaya::mh2_eft::threshold_loop_functions::F5, 1e-11);
+   test_1("F6", himalaya::mh2_eft::threshold_loop_functions::F6, 1e-11);
+   test_1("F7", himalaya::mh2_eft::threshold_loop_functions::F7, 1e-11);
+}
+
+
+TEST_CASE("test_2")
+{
+   test_2("F8", himalaya::mh2_eft::threshold_loop_functions::F8, 1e-8);
+   test_2("F9", himalaya::mh2_eft::threshold_loop_functions::F9, 1e-11);
+}
+
 
 TEST_CASE("test_3")
 {
